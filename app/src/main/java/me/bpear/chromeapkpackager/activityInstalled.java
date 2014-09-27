259cdabfe4a844cfaafb12297d718df948a21285
Brandon Pearse <bpear96@gmail.com>
2014-09-27 10:40:17 -0400

    Back button on device now prompts user if they want to restart the wizard.

--- app/src/main/java/me/bpear/chromeapkpackager/activityInstalled.java
+++ app/src/main/java/me/bpear/chromeapkpackager/activityInstalled.java
@@ -1,7 +1,9 @@
 package me.bpear.chromeapkpackager;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
@@ -407,6 +409,31 @@ public class activityInstalled extends Activity {
         finish(); // End dialog activity to return to app wizard.
 
         //TODO: Cleanup leftover directories (Unzipped app directory, pulled app directory are not needed.)
+    }
+
+    @Override
+    public void onBackPressed() {
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case DialogInterface.BUTTON_POSITIVE:
+                        //Yes button clicked
+                        Intent intent = new Intent(activityInstalled.this, activityWizard.class);
+                        finish();
+                        startActivity(intent);
+                        break;
+
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        //No button clicked
+                        break;
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Back button pressed. Do you want to restart the wizard?").setPositiveButton("Yes", dialogClickListener)
+                .setNegativeButton("Cancel", dialogClickListener).show();
     }
 
 }
--- app/src/main/java/me/bpear/chromeapkpackager/activityWizard.java
+++ app/src/main/java/me/bpear/chromeapkpackager/activityWizard.java
@@ -1,5 +1,8 @@
 package me.bpear.chromeapkpackager;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
@@ -10,5 +13,30 @@ public class activityWizard extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wizard);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case DialogInterface.BUTTON_POSITIVE:
+                        //Yes button clicked
+                        Intent intent = getIntent();
+                        finish();
+                        startActivity(intent);
+                        break;
+
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        //No button clicked
+                        break;
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Back button pressed. Do you want to restart the wizard?").setPositiveButton("Yes", dialogClickListener)
+                .setNegativeButton("Cancel", dialogClickListener).show();
     }
 }
