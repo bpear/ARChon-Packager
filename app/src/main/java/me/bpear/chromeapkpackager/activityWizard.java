90509ea9e38877c5e43f3ee682092ec6d76866b2
Brandon Pearse <bpear96@gmail.com>
2014-09-27 18:20:33 -0400

    Clean up.

--- app/src/main/java/me/bpear/chromeapkpackager/activityWizard.java
+++ app/src/main/java/me/bpear/chromeapkpackager/activityWizard.java
@@ -21,27 +21,26 @@ public class activityWizard extends FragmentActivity {
     @Override
     public void onBackPressed() {
         if (g.getstep() == 1) {
-            if (g.getstep() == 1) {
-                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        switch (which) {
-                            case DialogInterface.BUTTON_POSITIVE:
-                                //Yes button clicked
-                                finish();
-                                break;
+            DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    switch (which) {
+                        case DialogInterface.BUTTON_POSITIVE:
+                            //Yes button clicked
+                            finish();
+                            break;
 
-                            case DialogInterface.BUTTON_NEGATIVE:
-                                //No button clicked
-                                break;
-                        }
+                        case DialogInterface.BUTTON_NEGATIVE:
+                            //No button clicked
+                            break;
                     }
-                };
+                }
+            };
 
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setMessage("Back button pressed. Do you want to exit?").setPositiveButton("Yes", dialogClickListener)
-                        .setNegativeButton("no", dialogClickListener).show();
-            }
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Back button pressed. Do you want to exit?").setPositiveButton("Yes", dialogClickListener)
+                    .setNegativeButton("no", dialogClickListener).show();
+            
         }
         if (g.getstep() == 2) {
             recreate();
