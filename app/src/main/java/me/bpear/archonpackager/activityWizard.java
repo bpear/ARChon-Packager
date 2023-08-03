2a27da2d69e6ee6c8e2fb0d04f43443b07c595ae
Willy FRANÇOIS <wfrancois45@gmail.com>
2023-08-03 21:40:02 +0200

    Basic upgrade to build the project again

--- app/src/main/java/me/bpear/archonpackager/activityWizard.java
+++ app/src/main/java/me/bpear/archonpackager/activityWizard.java
@@ -7,7 +7,8 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+
+import androidx.fragment.app.FragmentActivity;
 
 import com.readystatesoftware.systembartint.SystemBarTintManager;
 
